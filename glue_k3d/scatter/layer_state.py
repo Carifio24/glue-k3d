35b367b5717831aaafef0e64e2133bf124bcc77f
Carifio24 <carifio24@gmail.com>
2025-10-14 11:31:53 -0400

    Update scatter shader options.

--- glue_k3d/scatter/layer_state.py
+++ glue_k3d/scatter/layer_state.py
@@ -7,4 +7,4 @@ class K3DScatterLayerState(ScatterLayerState):
 
     def __init__(self, viewer_state=None, layer=None, **kwargs):
         super().__init__(viewer_state, layer, **kwargs) 
-        K3DScatterLayerState.shader.set_choices(self, ["3d", "3dSpecular", "flat", "simple", "dot"])
+        K3DScatterLayerState.shader.set_choices(self, ["3d", "3dSpecular", "flat", "mesh", "dot"])
