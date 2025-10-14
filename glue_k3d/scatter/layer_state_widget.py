2f75dfa03e73f33f37c7eccf0053dadfdaf882ff
Carifio24 <carifio24@gmail.com>
2025-10-14 14:27:04 -0400

    Move state widgets into their own files.

---
+++ glue_k3d/scatter/layer_state_widget.py
@@ -0,0 +1,46 @@
+from ipywidgets import FloatSlider, VBox
+from glue_jupyter.link import link
+from glue_jupyter.widgets import LinkedDropdown, Color, Size
+
+class Scatter3DLayerStateWidget(VBox):
+
+    def __init__(self, layer_state):
+
+        self.state = layer_state
+
+        self.widget_opacity = FloatSlider(min=0, max=1, step=0.01, value=self.state.alpha, description="Opacity")
+        link((self.state, 'alpha'), (self.widget_opacity, 'value'))
+
+        self.widget_shader = LinkedDropdown(self.state, 'shader', label="Shader")
+        link((self.state, 'shader'), (self.widget_shader, 'value'))
+
+        self.widget_size = Size(state=self.state)
+        self.widget_color = Color(state=self.state)
+
+        # self.widget_shininess = FloatSlider(min=0, max=100, value=self.state.shininess, label="Shininess")
+        # link((self.state, 'shininess'), (self.widget_shininess, 'value'))
+
+        # self.widget_mesh_detail = IntSlider(min=0, max=10, value=self.state.mesh_detail, label="Mesh Detail")
+        # link((self.state, 'mesh_detail'), (self.widget_mesh_detail, 'value'))
+        # vector/quivers
+        # self.widget_vector = Checkbox(description='show vectors', value=self.state.vector_visible)
+
+        # self.widget_vector_x = LinkedDropdown(self.state, 'vx_att', label='vx')
+        # self.widget_vector_y = LinkedDropdown(self.state, 'vy_att', label='vy')
+        # self.widget_vector_z = LinkedDropdown(self.state, 'vz_att', label='vz')
+
+        # link((self.state, 'vector_visible'), (self.widget_vector, 'value'))
+        # dlink((self.widget_vector, 'value'), (self.widget_vector_x.layout, 'display'),
+        #       lambda value: None if value else 'none')
+        # dlink((self.widget_vector, 'value'), (self.widget_vector_y.layout, 'display'),
+        #       lambda value: None if value else 'none')
+        # dlink((self.widget_vector, 'value'), (self.widget_vector_z.layout, 'display'),
+        #       lambda value: None if value else 'none')
+
+        # link((self.state, 'vector_visible'), (self.widget_vector, 'value'))
+
+        super().__init__([self.widget_opacity, self.widget_shader,
+                          self.widget_size, self.widget_color])
+                         # self.widget_shininess, self.widget_mesh_detail])
+                         # self.widget_vector, self.widget_vector_x,
+                         # self.widget_vector_y, self.widget_vector_z])
